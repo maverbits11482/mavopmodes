467283bacf8122eb5347178191c1d835b451e407
doc_crate <cait.h.crawford@gmail.com>
2017-02-19 16:15:20 -0500

    Added MavBot class and test opmode.

---
+++ MavBot.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by henry & tom on 2/19/17.
+ */
+
+public class MavBot {
+
+    // Construction.
+    public MavBot() {
+
+    }
+
+    // Constants.
+    public static final String MOTOR_LEFT_NAME="motorLeft";
+    public static final String MOTOR_RIGHT_NAME="motorRight";
+    public static final DcMotor.RunMode DEFAULT_MOTOR_MODE = DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+
+
+    // member variables
+    public DcMotor motorLeft;
+    public DcMotor motorRight;
+
+    // current drive mode.
+    public MavBotDriveMode currDriveMode;
+
+    // MavBot drive modes.
+    public enum MavBotDriveMode {
+        DRIVE_MODE_TANK,
+        DRIVE_MODE_POV
+    }
+
+    private HardwareMap hardwareMap;
+
+    // this method initializes and preconfigures the robot hardware.
+    public void initialize(HardwareMap origMap) {
+        // get reference to the hardware map for this robot.
+        // this reference should be passed as an argument in the initialize method.
+        hardwareMap = origMap;
+
+        // get references to the motors
+        motorLeft = hardwareMap.get(DcMotor.class, MOTOR_LEFT_NAME);
+        motorRight = hardwareMap.get(DcMotor.class, MOTOR_RIGHT_NAME);
+
+        // reverse direction of right motor
+        motorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // reset encoders.
+        motorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        // set to our default motor mode.
+        motorRight.setMode(DEFAULT_MOTOR_MODE);
+        motorLeft.setMode(DEFAULT_MOTOR_MODE);
+
+        // set default drive mode.
+        currDriveMode = MavBotDriveMode.DRIVE_MODE_TANK;
+    }
+
+    // this method processes game input and moves robot
+    public void gamepadDrive(Gamepad gamepad) {
+        if(currDriveMode == MavBotDriveMode.DRIVE_MODE_TANK) {
+            motorLeft.setPower(-gamepad.left_stick_y);
+            motorRight.setPower(-gamepad.right_stick_y);
+        }
+
+    }
+
+}
+
---
+++ TomTestOpMode.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by cait on 2/19/17.
+ */
+
+@TeleOp(name="Tom Test Op Mode", group="maverbits")
+public class TomTestOpMode extends LinearOpMode {
+
+    // a reference to our robot hardware.
+    public MavBot mavbot;
+
+
+    public void runOpMode()  {
+
+        // create a new object that represents our robot hardware.
+        telemetry.addData("status", "creating new MavBot object...");
+        telemetry.update();
+        mavbot = new MavBot();
+
+        // initialize our robot.
+        mavbot.initialize(hardwareMap);
+
+        // wait until driver pushes start button
+        telemetry.addData("status", "Press start button to begin...");
+        telemetry.update();
+        waitForStart();
+
+        // loop while the op mode is active
+        while(opModeIsActive()) {
+
+             // run the drive method
+            mavbot.gamepadDrive(gamepad1);
+
+            // use telemetry to send info back to drive.
+            telemetry.addData("LeftPower", mavbot.motorLeft.getPower());
+            telemetry.addData("RightPower", mavbot.motorLeft.getPower());
+            telemetry.addData("Current Drive Mode", mavbot.currDriveMode);
+            telemetry.addData("Left Position", mavbot.motorLeft.getCurrentPosition());
+            telemetry.addData("Right Position", mavbot.motorRight.getCurrentPosition());
+            telemetry.update();
+
+            // idle a moment, to give the robot controller a chance to do other parallel tasks.
+            idle();
+
+        }
+
+    }
+
+}
